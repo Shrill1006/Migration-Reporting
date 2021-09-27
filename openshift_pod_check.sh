fe3f356f64a082ae5a94c55bf208c794ac43f028
Shrill Patel <T971998@corp.ads>
2021-09-27 15:12:01 -0400

    Changed script to take in datacenter as a parameter

--- openshift_pod_check.sh
+++ openshift_pod_check.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# Datacenter constants
+QIDC_MASTER="paas-master-east-np.tsl.telus.com"
+KIDC_MASTER="paas-master-west-np.tsl.telus.com"
+QIDC311_MASTER="paas-master-east2-np.tsl.telus.com"
+KIDC311_MASTER="paas-master-west2-np.tsl.telus.com"
+
+# Take in string and make it all CAPS
+uppercase() {
+  echo "$1" | tr a-z A-Z >/dev/null
+}
+
 # Login and get current namespaces
 get_namespaces() {
-	login $1 $2
+	change_context $1 $2
     IFS=$'\n' read -r -d '' -a proj_names < <( oc get projects -o custom-columns=NAME:.metadata.name --no-headers && printf '\0' )
 }
 
@@ -22,9 +33,11 @@ migrated() {
 	fi
 }
 
-# Login to a namespace
-login() {
-	oc login $1 $2 >/dev/null
+change_context() {
+	CLUSTER=$1
+	PORT=$2
+	CONTEXT=`echo $CLUSTER | sed 's/\./-/g'`
+	oc config use-context openshift/${CONTEXT}:${PORT}/system:serviceaccount:openshift:project-view >/dev/null
 }
 
 # Logout of current project
@@ -34,48 +47,52 @@ logout() {
 
 # This function checks if namespaces have been migrated to newer version of OpenShift
 migration_check() {
-	# Check if proper number of args are provided
-	if [ "$#" -ne 4 ]
+	# Retrieve all namespaces under the older project
+	get_namespaces $SOURCE 443
+	declare -p proj_names >/dev/null
+	arr1=( "${proj_names[@]}" )
+
+	# Retrieve all namespaces under the newer project
+	get_namespaces $DEST 443
+	declare -p proj_names >/dev/null
+	arr2=( "${proj_names[@]}" )
+
+	# From all the namespaces between the new and old projects, extract all namespaces that are the same between both projects
+	common_names=($(comm -12 <(printf '%s\n' "${arr1[@]}" | LC_ALL=C sort) <(printf '%s\n' "${arr2[@]}" | LC_ALL=C sort)))
+	if [ "${#common_names[@]}" -eq 0 ]
 	then
-		echo "Illegal number of parameters!"
-	else
-		# Retrieve all namespaces under the older project
-		get_namespaces $1 $2
-		declare -p proj_names >/dev/null
-		arr1=( "${proj_names[@]}" )
-
-		# Retrieve all namespaces under the newer project
-		get_namespaces $3 $4
-		declare -p proj_names >/dev/null
-		arr2=( "${proj_names[@]}" )
-
-		# From all the namespaces between the new and old projects, extract all namespaces that are the same between both projects
-		common_names=($(comm -12 <(printf '%s\n' "${arr1[@]}" | LC_ALL=C sort) <(printf '%s\n' "${arr2[@]}" | LC_ALL=C sort)))
-		if [ "${#common_names[@]}" -eq 0 ]
-		then
-			echo "No common namespaces to check for migration!"
-			exit 1
-		fi
-
-		declare -a running_pods_1=()
-		declare -a running_pods_2=()
-		# For each common namespace, retrieve the number of running pods
-		for name in ${common_names[@]}
-		do
-			login $1 $2
-			running_pods_1+=($(get_num_of_running_pods $name))
-			login $3 $4
-			running_pods_2+=($(get_num_of_running_pods $name))
-		done
-		logout
-
-		# Based on the number of running pods for each namespace, determine if the project has been completelty migrated
-		for ((i=0; i< ${#common_names[@]}; i++))
-		do
-			migrated $((${running_pods_1[$i]})) $((${running_pods_2[$i]})) ${common_names[$i]}
-		done
+		echo "No common namespaces to check for migration!"
+		exit 1
 	fi
+
+	declare -a running_pods_1=()
+	declare -a running_pods_2=()
+	# For each common namespace, retrieve the number of running pods
+	for name in ${common_names[@]}
+	do
+		change_context $SOURCE 443
+		running_pods_1+=($(get_num_of_running_pods $name))
+		change_context $DEST 443
+		running_pods_2+=($(get_num_of_running_pods $name))
+	done
+	logout
+
+	# Based on the number of running pods for each namespace, determine if the project has been completelty migrated
+	for ((i=0; i< ${#common_names[@]}; i++))
+	do
+		migrated $((${running_pods_1[$i]})) $((${running_pods_2[$i]})) ${common_names[$i]}
+	done
 }
 
 #------ Execution of script begins here ------
-migration_check $1 $2 $3 $4
+DATACENTER=`uppercase $1`
+if [ $DATACENTER == "QIDC" ]
+then
+	SOURCE=$QIDC_MASTER
+	DEST=$QIDC311_MASTER
+else
+	SOURCE=$KIDC_MASTER
+	DEST=$KIDC311_MASTER
+fi
+
+migration_check
